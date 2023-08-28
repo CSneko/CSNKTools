c254957ed2df8fbf6e49c71c460f1349793e8faa
CrystalNeko <minecrafthetun@outlook.com>
2023-08-28 13:01:28 +0800

    Update CSNKTools.java

--- src/main/java/com/crystalneko/csnktools/csnktools/CSNKTools.java
+++ src/main/java/com/crystalneko/csnktools/csnktools/CSNKTools.java
@@ -1,5 +1,6 @@
 package com.crystalneko.csnktools.csnktools;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CSNKTools extends JavaPlugin {
@@ -7,11 +8,32 @@ public final class CSNKTools extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        Bukkit.getConsoleSender().sendMessage(
+                "//--------------| |---------=---------|\n" +
+                "||--------------| |---------=---------|\n" +
+                "||\t\t\t   | |\n" +
+                "||\t\t\t   | |\n" +
+                "||\t\t\t   | |\n" +
+                "||\t\t\t   | |\n" +
+                "||\t\t\t   | |\n" +
+                "||\t\t\t   | |\n" +
+                "||--------------|\t   | |\n" +
+                "\\\\--------------|\t   ---" + "\nCSNKTools V0.0.1 作者:CrystalNeko");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        Bukkit.getConsoleSender().sendMessage(
+                "//--------------| |---------=---------|\n" +
+                        "||--------------| |---------=---------|\n" +
+                        "||\t\t\t   | |\n" +
+                        "||\t\t\t   | |\n" +
+                        "||\t\t\t   | |\n" +
+                        "||\t\t\t   | |\n" +
+                        "||\t\t\t   | |\n" +
+                        "||\t\t\t   | |\n" +
+                        "||--------------|\t   | |\n" +
+                        "\\\\--------------|\t   ---" + "\nCSNKTools V0.0.1 作者:CrystalNeko");
     }
 }
